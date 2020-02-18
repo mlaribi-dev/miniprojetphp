f40de6a83a7d52e515f223bd992c4e6314ad2aa7
mlaribi-dev <61144278+mlaribi-dev@users.noreply.github.com>
2020-02-18 10:50:29 +0100

    Update index.php
    modification index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 ?>
 	<main>
 		<h1>Accueil</h1>
-		<p>Lorem ipsum dolor sit amet sit amet, consectetur adipisicing elit. Sint vitae sit adipisci quos possimus, eveniet odit quisquam? Porro molestias modi impedit nesciunt similique dolorum, deleniti atque quas temporibus ut accusantium!</p>
+		<p>Lorem ipsum dolor sit amet sit amet ispum , consectetur adipisicing elit. Sint vitae sit adipisci quos possimus, eveniet odit quisquam? Porro molestias modi impedit nesciunt similique dolorum, deleniti atque quas temporibus ut accusantium!</p>
 	</main>
 
 <?php require_once('./includes/footer.php'); ?>
